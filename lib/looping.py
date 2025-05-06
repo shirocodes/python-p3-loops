0d744cbdf2b44502c3df62b866ccff26f94600ed
shirocodes <ishirapt@gmail.com>
2025-05-06 19:42:44 +0300

    Completed the lab.

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -=1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return[x ** 2 for x in int_list]
+print(square_integers([1, 2, 3, 4]))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):  # Loop through numbers from 1 to 100
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")  # For multiples of both 3 and 5
+        elif i % 3 == 0:
+            print("Fizz")  # For multiples of 3
+        elif i % 5 == 0:
+            print("Buzz")  # For multiples of 5
+        else:
+            print(i)  # For all other numbers
+print(fizzbuzz())
